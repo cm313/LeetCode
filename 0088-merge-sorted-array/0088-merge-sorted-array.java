b9307e21b821da366d1907b5b8ad881fa1495924
Chanda Chandra Mahesh <93776284+cm313@users.noreply.github.com>
2024-03-30 08:47:44 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (72.57%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int f_pointer = 0;
+        // brutefore solution
+    /*    int f_pointer = 0;
         int s_pointer = 0;
         for(int i=0; i<m; i++){
            f_pointer++;
@@ -10,5 +11,19 @@ class Solution {
             s_pointer++;
         }
         Arrays.sort(nums1);
+        */
+        // optimal solution
+
+        int  f_po = m-1;
+        int  s_po = n-1;
+        int t_po = m+n-1;
+        while(s_po>=0){
+          if(f_po>=0 && nums1[f_po]>nums2[s_po]){
+             nums1[t_po--] = nums1[f_po--];
+          }
+            else{
+                nums1[t_po--] = nums2[s_po--];
+            }
+        }
     }
 }
