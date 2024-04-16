29cb06145ebd5817cdbfa8721241a7aca90149ff
Chanda Chandra Mahesh <93776284+cm313@users.noreply.github.com>
2024-04-16 08:16:33 +0530

    Time: 2 ms (32.09%), Space: 57.4 MB (28.27%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,52 +1,14 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        //below code is not working ,,needs to update
-       /* int len=nums.length;
-        int[] arr=new int[len];
-        for(int i=0;i<len;i++){
-            if(i+k<len){
-                arr[i+k]=nums[i];
-            }
+     int n = nums.length;
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[(i+k)%n] = nums[i];
         }
-        for(int i=0;i<k;i++){
-             else
-            arr[i]=nums[k+i+1];
+        int  j =0;
+        for(int val: arr){
+            nums[j] = val;
+            j++;
         }
-        int i=0;
-        for(int val:arr){
-            nums[i++]=val;
-        } 
-
-        // working code
-      int len=nums.length;
-      int arr[]=new int[len];
-      for(int i=0;i<len;i++){
-          arr[(i+k)%len]=nums[i];
-      }
-         int i=0;
-        for(int val:arr){
-            nums[i++]=val;
-        } 
-        // my approach accepted but when submitting given TLE
-        int i=0;
-        while(i<k){
-          int n = nums.length;
-            int temp = nums[n-1];
-            for(int j=n-2; j>=0; j--){
-                nums[j+1] = nums[j];
-            }
-            nums[0] = temp;
-            i++;
-        } */
-        
-         int len=nums.length;
-      int arr[]=new int[len];
-      for(int i=0;i<len;i++){
-          arr[(i+k)%len]=nums[i];
-      }
-         int i=0;
-        for(int val:arr){
-            nums[i++]=val;
-        } 
-    }
+ }       
 }
