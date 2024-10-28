202b75b307b56a8c31c53cc0f3fbc52b77f1a031
Chanda Chandra Mahesh <93776284+cm313@users.noreply.github.com>
2024-10-28 22:00:49 +0530

    Time: 5 ms (32.81%), Space: 71.9 MB (94.47%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,20 +1,30 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-      int n = nums.length;
-        int[] arr = new int[n];
-        int i=0;
-        int even_index = 0, odd_index=1;
-        while(i<n){
-            if(nums[i] > 0){
-                arr[even_index] = nums[i];
-                even_index += 2;
-            }
-            else if(nums[i] < 0){
-                arr[odd_index] = nums[i];
-               odd_index += 2;
-            }
-            i++;
-            }
-        return arr;
+      int n = nums.length; 
+      int temp1=0,temp2=0;
+      int[] pos = new int[n/2];
+      int[] neg = new int[n/2];
+      for(int i=0; i<n; i++){
+        if(nums[i] >0){
+           pos[temp1] = nums[i];
+           temp1++;;
+        }
+        else if(nums[i]<0){
+            neg[temp2] = nums[i];
+            temp2++;
+        }
+      }
+      int temp3=0,temp4=0;
+      for(int i=0; i<n; i++){
+        if((i%2) == 0){
+            nums[i] = pos[temp3];
+            temp3++;
+        }
+        else{
+            nums[i] = neg[temp4];
+            temp4++;
+        }
+      }
+      return nums;
     }
 }
