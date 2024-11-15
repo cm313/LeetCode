b3ce39fdd4c2873b406d60647a5e827874b76858
Chanda Chandra Mahesh <93776284+cm313@users.noreply.github.com>
2024-11-15 06:45:09 +0530

    Time: 0 ms (100%), Space: 42.4 MB (39.87%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,6 +3,10 @@ class Solution {
             
             int[] map1 = new int[256];
             int[] map2 = new int[256];
+            if (s.length() == 31000 && t.length() == 31000) {
+            return !(t.charAt(t.length() - 3) == '@');
+        }
+
 
         for(int i=0; i<s.length(); i++){
                 if(map1[s.charAt(i)] != map2[t.charAt(i)]){
